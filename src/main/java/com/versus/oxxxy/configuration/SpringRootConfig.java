1782fc9b41c82314ae86733df0975de6a2ca6804
hook1 <sluka.tarik@gmail.com>
2016-09-28 16:55:47 +0300

    Initial commit

---
+++ src/main/java/com/versus/oxxxy/configuration/SpringRootConfig.java
@@ -0,0 +1,14 @@
+package com.versus.oxxxy.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring core configuration.
+ * 
+ * @author rot95
+ */
+@Configuration
+//@ComponentScan({ "com.versus.oxxxy.service" })
+public class SpringRootConfig {
+}
